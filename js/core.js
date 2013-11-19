0f62ffa4a9677234832af46c2fa74fae049723bb
Josh Sherman <josh@gravityblvd.com>
2013-11-19 01:56:12 -0500

    De-bootstrap-ified

--- js/core.js
+++ js/core.js
@@ -7,14 +7,14 @@ $(function()
 
 		$('.year').removeClass('year-active').removeClass('year-inactive');
 		$('.posts-' + year).scrollTop(0);
-		$('.navbar-header').removeClass('navbar-inactive');
+		$('header').removeClass('header-inactive');
 
 		if (!is_active)
 		{
 			$(this).addClass('year-active');
 			$('.posts-' + year).addClass('year-active');
 			$('.year:not(.year-active)').addClass('year-inactive');
-			$('.navbar-header').addClass('navbar-inactive');
+			$('header').addClass('header-inactive');
 		}
 	});
 });
