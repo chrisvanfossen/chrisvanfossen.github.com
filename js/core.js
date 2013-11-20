52a5b3a0e0c60eaa109a7a5f45e1d6c72793622b
Josh Sherman <josh@gravityblvd.com>
2013-11-19 22:30:37 -0500

    Fixed jumping on smaller screens
    Found a bug as well, got that all cleaned up

--- js/core.js
+++ js/core.js
@@ -4,6 +4,7 @@ $(function()
 	{
 		var is_active = $(this).hasClass('year-active');
 		var year      = $(this).data('year');
+		var fragment  = '#';
 
 		$('.year').removeClass('year-active').removeClass('year-inactive');
 		$('.posts-' + year).scrollTop(0);
@@ -12,9 +13,15 @@ $(function()
 		if (!is_active)
 		{
 			$(this).addClass('year-active');
-			$('.posts-' + year).addClass('year-active');
 			$('.year:not(.year-active)').addClass('year-inactive');
 			$('header').addClass('header-inactive');
+
+			fragment = fragment + year;
+		}
+
+		if (window.innerWidth <= 1024)
+		{
+			location.href = fragment;
 		}
 	});
 });
